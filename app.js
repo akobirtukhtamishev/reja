8afeed9192180fa09c2ed85d8b119d930a7a10f2
akobirtukhtamishev <akobirtukhtamishev@gmail.com>
2026-09-02 03:35:36 +0900

    feat:MongoDB connection logic

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("Web serverni boshlash uchun 'npm start' buyrug'ini ishlating");
+const express = require('express');
+const app = express();
+
+// MongoDB call - chaqirish
+const db = require("./server").db();
+
+// malumot shartli ravishda 4 ta bosqishga bolinadi
+// 1. (Kirish code) Expressga kirib kelayotgan malumotlarga bogliq bolganf kodlar yoziladi
+app.use(express.static("public")); // browserdan kirib kelayotgan zaproslar uchun public folder ochiq ekanligini korsatadi
+app.use(express.json());  // kirib kelayotgan json formatdagi datani object holatiga o'girib beradi
+app.use(express.urlencoded({extended: false})); //agar buni yozmasak html formdan post qilingan narsalarni express qabul qilmaydi
+
+// 2. (Session code)
+
+// 3. (Views code) biz backendda html-front end yaratamiz buni uchun ejs dan foydalanamiz -> npm i ejs
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4. Routing code
+app.post("/create-item", (req, res) => {
+    console.log({ ...req.body });
+    res.json({test: "success"});
+});
+
+app.get("/", function(req, res) {
+    res.render("harid");
+});
+
+app.get("/gift", function(req, res) {
+    res.end("<h1>siz sovgalar sahifasidaasiz</h1>");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,36 +1,22 @@
-console.log("Web serverni boshlash uchun 'npm start' buyrug'ini ishlating");
-const express = require('express');
-const app = express();
 const http = require("http");
+const mongodb = require("mongodb");
 
-// malumot shartli ravishda 4 ta bosqishga bolinadi
-// 1. (Kirish code) Expressga kirib kelayotgan malumotlarga bogliq bolganf kodlar yoziladi
-app.use(express.static("public")); // browserdan kirib kelayotgan zaproslar uchun public folder ochiq ekanligini korsatadi
-app.use(express.json());  // kirib kelayotgan json formatdagi datani object holatiga o'girib beradi
-app.use(express.urlencoded({extended: false})); //agar buni yozmasak html formdan post qilingan narsalarni express qabul qilmaydi
+let db;
+const connectionString = "mongodb+srv://ethan_2408:N0rth-r3m3mb3r@cluster0.dorhfsw.mongodb.net/";
 
-// 2. (Session code)
-
-// 3. (Views code) biz backendda html-front end yaratamiz buni uchun ejs dan foydalanamiz -> npm i ejs
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4. Routing code
-app.post("/create-item", (req, res) => {
-    console.log({ ...req.body });
-    res.json({test: "success"});
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}, (err, client) => {
+    if (err) console.log("Error on connection MongoDB");
+    else{
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function() {
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+});
+    }
 });
 
-app.get("/", function(req, res) {
-    res.render("harid");
-});
-
-app.get("/gift", function(req, res) {
-    res.end("<h1>siz sovgalar sahifasidaasiz</h1>");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
